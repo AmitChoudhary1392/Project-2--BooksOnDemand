6d50d8fc159d776f1a2305c306be21e5542971d4
Alessandra <52939705+alefaraujo-ca@users.noreply.github.com>
2020-02-10 21:41:42 -0500

    Models

--- BookShare App_testversion/models.py
+++ BookShare App_testversion/models.py
@@ -10,7 +10,7 @@ class Book(db.Model):
     isbn = db.Column(db.String(13))
     image_url = db.Column(db.String(1000))
     author = db.Column(db.String(500))
-    published_date = db.Column(db.Date)
+    published_date = db.Column(db.String(10))
     publisher = db.Column(db.String(100))
     language = db.Column(db.String(2))
 
@@ -22,13 +22,14 @@ class Owner(db.Model):
     __tablename__ = 'Owner'
 
     id_book = db.Column(db.String(20), primary_key=True) 
-    title = db.Column(db.String(300))
     owner_email = db.Column(db.String(60), primary_key=True)
     rating = db.Column(db.Integer)
     review = db.Column(db.String(1000))
     location = db.Column(db.String(150))
     lat= db.Column(db.Float(10))
     lng= db.Column(db.Float(10))
+    contact_details = db.Column(db.String(1000))
+    available = db.Column(db.Integer)
 
     def __repr__(self):
         return '<Owner %r>' % (self.owner_email)
